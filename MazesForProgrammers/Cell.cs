6fbadaf0a01dd6b5ac55b86be228f65690bd4302
Jamie <jsams@fallingsloth.uk>
2019-04-05 22:13:47 +0100

    Revert "Implemented the Cell class."
    This reverts commit 39f9c5d75d623b33f415833d283727cb33d6e6a9.

--- MazesForProgrammers/Cell.cs
+++ MazesForProgrammers/Cell.cs
@@ -4,63 +4,8 @@ using System.Text;
 
 namespace MazesForProgrammers
 {
-    public class Cell
+    public struct Cell
     {
-        public int column { get; set; }
-        public int row { get; set; }
 
-        public Cell north { get; set; }
-        public Cell east { get; set; }
-        public Cell south { get; set; }
-        public Cell west { get; set; }
-
-        public Dictionary<Directions, Cell> Neighbours
-        {
-            get
-            {
-                var rv = new Dictionary<Directions, Cell>();
-                rv.Add(Directions.North, north);
-                rv.Add(Directions.East, east);
-                rv.Add(Directions.South, south);
-                rv.Add(Directions.West, west);
-                return rv;
-            }
-        }
-
-        List<Cell> _links;
-        public Cell[] links
-        {
-            get
-            {
-                return _links.ToArray();
-            }
-        }
-
-        public Cell(int column, int row)
-        {
-            this.column = column;
-            this.row = row;
-
-            this._links = new List<Cell>();
-        }
-
-        public void Link(Cell other, bool bidi = true)
-        {
-            _links.Add(other);
-            if (bidi)
-                other.Link(this);
-        }
-
-        public void Unlink(Cell other, bool bidi = true)
-        {
-            _links.Remove(other);
-            if (bidi)
-                other.Unlink(this);
-        }
-
-        public bool IsLinked(Cell other)
-        {
-            return _links.Contains(other);
-        }
     }
 }

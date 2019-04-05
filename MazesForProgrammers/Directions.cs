39f9c5d75d623b33f415833d283727cb33d6e6a9
Jamie <jsams@fallingsloth.uk>
2019-04-05 14:50:37 +0100

    Implemented the Cell class.

---
+++ MazesForProgrammers/Directions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MazesForProgrammers
+{
+    public enum Directions
+    {
+        Null    = 0,
+        North   = 1,
+        East    = 2,
+        South   = 4,
+        West    = 8
+    }
+}

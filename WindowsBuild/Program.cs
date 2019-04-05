39a0236db6016202957574ecb6ecb5bfa8eadca9
Jamie <jsams@fallingsloth.uk>
2019-04-05 01:19:13 +0100

    Switched to Monogame because I need to get out of the Unity comfort zone.

---
+++ WindowsBuild/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WindowsBuild
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MazesForProgrammers.MainGame())
+                game.Run();
+        }
+    }
+#endif
+}

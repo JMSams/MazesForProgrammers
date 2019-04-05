2d76cce2433c293efbab39064565b94aa5e0bd94
Jamie <jsams@fallingsloth.uk>
2019-04-05 22:13:53 +0100

    Revert "Switched to Monogame because I need to get out of the Unity comfort zone."
    This reverts commit 39a0236db6016202957574ecb6ecb5bfa8eadca9.

---
+++ Assets/Scripts/Highlight.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mazes_for_Programmers
+{
+    public struct Highlight
+    {
+        public List<Cell> cells;
+        public Color colour;
+    }
+}

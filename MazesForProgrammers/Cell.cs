655a80b71707cac8d3c9254ff06dc9c281aec4d2
Jamie <jsams@fallingsloth.uk>
2019-04-10 12:57:55 +0100

    Started a mono branch, hoping it will be easier to draw primitives.

---
+++ MazesForProgrammers/Cell.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using MonoGame;
+
+namespace MazesForProgrammers
+{
+    class Cell
+    {
+        Grid parent;
+
+        public int column { get; protected set; }
+        public int row { get; protected set; }
+
+        public Cell north { get { return parent[column, row + 1]; } }
+        public Cell east { get { return parent[column + 1, row]; } }
+        public Cell south { get { return parent[column, row - 1]; } }
+        public Cell west { get { return parent[column - 1, row]; } }
+
+        List<Cell> links;
+
+        public Cell(int column, int row, Grid parent)
+        {
+            this.column = column;
+            this.row = row;
+            this.parent = parent;
+            links = new List<Cell>();
+        }
+
+        public void Link(Cell other, bool bidi = true)
+        {
+            links.Add(other);
+            if (bidi)
+                other.Link(this, false);
+        }
+
+        public void Draw()
+        {
+            // http://rbwhitaker.wikidot.com/drawing-triangles
+        }
+    }
+}
---
+++ MazesForProgrammers/Grid.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MazesForProgrammers
+{
+    class Grid
+    {
+        public int columnCount { get; protected set; }
+        public int rowCount { get; protected set; }
+
+        Cell[,] cells;
+
+        public Grid(int columnCount, int rowCount)
+        {
+            this.columnCount = columnCount;
+            this.rowCount = rowCount;
+
+            cells = new Cell[columnCount, rowCount];
+        }
+
+        public Cell this[int column, int row]
+        {
+            get
+            {
+                if (column < 0
+                    || row < 0
+                    || column >= columnCount
+                    || row >= rowCount)
+                    return null;
+                else
+                    return cells[column, row];
+            }
+        }
+    }
+}

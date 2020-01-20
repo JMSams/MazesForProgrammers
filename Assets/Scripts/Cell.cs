f69c425e6f48ee200840f123e0bab2b8961b7ed8
JMSams <sams_drum@hotmail.com>
2020-01-20 19:26:46 +0000

    New project

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,50 +1,19 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Mazes_for_Programmers
+public class Cell
 {
-    public class Cell
+    public Grid parent { get; protected set; }
+    public int row { get; protected set; }
+    public int column { get; protected set; }
+
+    public Cell(Grid parent, int row, int col)
     {
-        public Cell(int column, int row)
-        {
-            this.column = column;
-            this.row = row;
-            this.links = new List<Cell>();
-        }
-
-        public int row { get; protected set; }
-        public int column { get; protected set; }
-
-        public Cell north { get; set; }
-        public Cell east { get; set; }
-        public Cell south { get; set; }
-        public Cell west { get; set; }
-
-        public Color colour = Color.white;
-
-        public List<Cell> links;
-
-        public void Link(Cell other, bool bidi = true)
-        {
-            if (other == null) return;
-
-            links.Add(other);
-            if (bidi)
-                other.Link(this, false);
-        }
-
-        public void Unlink(Cell other, bool bidi = true)
-        {
-            if (other == null) return;
-
-            links.Remove(other);
-            if (bidi)
-                other.Unlink(this, false);
-        }
-
-        public bool IsLinked(Cell other)
-        {
-            return (other == null) ? false : links.Contains(other);
-        }
+        this.parent = parent;
+        this.row = row;
+        this.column = col;
     }
+
+
 }
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Grid
+{
+    public int columnCount { get; protected set; }
+    public int rowCount { get; protected set; }
+
+    protected virtual Cell[,] cells;
+
+    public Cell this[int row, int col]
+    {
+        get
+        {
+            if (row < 0 || row >= cells.GetLength(0) || col < 0 || col >= cells.GetLength(1))
+                return null;
+            else
+                return cells[row, col];
+        }
+        protected set => cells[row, col] = value;
+    }
+}
